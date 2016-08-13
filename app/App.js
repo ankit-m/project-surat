77f616e602add5cde87867142324556119fa3a25
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-13 15:31:53 +0530

    add react bootstrap and components

--- app/App.js
+++ app/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  foo() {
-    return 'good';
-  }
   render() {
-    return (
-      <div className="commentBox">
-          Hello,wosssld! I am a CommentBox.
-      </div>
-    );
+    return (<h1>hi</h1>);
   }
 }
