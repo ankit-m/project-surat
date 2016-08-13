54d623df3fdce91ab44758106b629c65e9071936
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-13 20:18:58 +0530

    Blob component

---
+++ app/components/Blob.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ListGroup, ListGroupItem, Button, Label } from 'react-bootstrap';
+
+const iconStyle = {
+  borderRadius: '40px',
+  margin: '2px',
+};
+
+const Blob = () => (
+  <ListGroup>
+    <ListGroupItem className="clearfix">
+      <div className="col-md-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit
+      </div>
+      <div className="pull-right">
+        <small className="text-muted" style={{ margin: '8px' }} >Kushan Joshi</small>
+        <span style={iconStyle}>
+          <Label bsStyle="warning"><small>100m</small></Label>
+        </span>
+        <span style={iconStyle}>
+          <Label bsStyle="default">
+            <span className="glyphicon glyphicon-lock" />
+          </Label>
+        </span>
+        <Button bsStyle="danger" style={iconStyle} bsSize="xsmall">
+          <span className="glyphicon glyphicon-remove" />
+        </Button>
+      </div>
+    </ListGroupItem>
+  </ListGroup>
+);
+
+export default Blob;
