33ba91e4994d6792c2de88c394b2563ab78b4f4b
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-14 01:40:53 +0530

    [wip] dynamic Blob component

--- app/components/Blob.js
+++ app/components/Blob.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, Button, Label } from 'react-bootstrap';
 
-const iconStyle = {
-  borderRadius: '40px',
-  margin: '2px',
+const styles = {
+  icon: {
+    borderRadius: '40px',
+    margin: '2px',
+  },
+  list: {
+    marginTop: '20px',
+  },
+  author: {
+    margin: '8px',
+  },
 };
 
-const Blob = () => (
-  <ListGroup style={{ marginTop: '20px' }}>
-    <ListGroupItem className="clearfix">
-      <div className="col-md-10">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit
-      </div>
-      <div className="pull-right">
-        <small className="text-muted" style={{ margin: '8px' }} >Kushan Joshi</small>
-        <span style={iconStyle}>
-          <Label bsStyle="warning"><small>100m</small></Label>
-        </span>
-        <span style={iconStyle}>
-          <Label bsStyle="default">
-            <span className="glyphicon glyphicon-lock" />
-          </Label>
-        </span>
-        <Button bsStyle="danger" style={iconStyle} bsSize="xsmall">
-          <span className="glyphicon glyphicon-remove" />
-        </Button>
-      </div>
-    </ListGroupItem>
-  </ListGroup>
-);
+const Blob = (props) => {
+  const a = 2;
+  return (
+    <ListGroup style={styles.list}>
+      <ListGroupItem className="clearfix">
+        <div className="col-md-9">
+          {props.data}
+        </div>
+        <div className="pull-right">
+          <small className="text-muted" style={styles.author} >{props.author}</small>
+          <span style={styles.icon}>
+            <Label bsStyle="warning"><small>{props.range} + 'm'</small></Label>
+          </span>
+          <span style={styles.icon}>
+            <Label bsStyle="default">
+              <span className="glyphicon glyphicon-lock" />
+            </Label>
+          </span>
+          <Button bsStyle="danger" style={styles.icon} bsSize="xsmall">
+            <span className="glyphicon glyphicon-remove" />
+          </Button>
+        </div>
+      </ListGroupItem>
+    </ListGroup>
+  );
+};
+
+Blob.propTypes = {
+  data: React.PropTypes.string.isRequired,
+  range: React.PropTypes.number.isRequired,
+  pass: React.PropTypes.number
+}
 
 export default Blob;
